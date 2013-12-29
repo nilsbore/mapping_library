0936a663c7934f3193419c4117b232bec17540df
Nils Bore <nbore@kth.se>
2013-12-29 16:39:31 +0100

    Leaner frequent subgraphs

--- graph_primitives/matlab/frequent_subgraphs.m
+++ graph_primitives/matlab/frequent_subgraphs.m
@@ -2,9 +2,18 @@
 addpath '/home/nbore/Installs/gboost-0.1.1/bin';
 folder = '/home/nbore/Workspace/mapping_library/graph_primitives/graphs/';
 load '/home/nbore/Workspace/mapping_library/graph_primitives/graphs/matgraphs.mat'
-[subg, count, GY] = gspan(G, 5, [4 0]);
-
+[subg, count, GY] = gspan(G, 10, [5 0]);
+[v, ind] = sort(count, 'descend');
+count = v;
+temp = cell(size(subg));
 n = length(subg);
+for i = 1:n
+    temp{i} = subg{ind(i)};
+end
+subg = temp;
+clear temp
+GY = GY(:, ind);
+
 for i = 1:n
     filename = [folder 'subg' sprintf('%.6d', i-1) '.dot'];
     mat2dot(subg{i}, filename);
